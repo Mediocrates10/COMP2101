8b69d81f3374ebe1a9e785a0b1c03c791839e2e9
Medioh <32582504+Mediocrates10@users.noreply.github.com>
2021-11-29 03:48:19 -0500

    Lab4

--- powershell/ipconfreport.ps1
+++ powershell/ipconfreport.ps1
@@ -1,11 +1,14 @@
 ﻿$table = foreach ($c in (get-ciminstance win32_networkadapterconfiguration | where ipenabled )) {
         new-object -typename psobject -property @{
-            Index = ($c).index
+                Index = ($c).index
         	Description = ($c).description
         	Address = ($c).ipaddress
         	Subnet = ($c).ipsubnet
+		
         	DNSDomain = ($c).dnsdomain
+	
 		DNSServer = ($c).dnsserver
     	}
     }
-    $table | format-table
+    $table | format-table -autosize
+
