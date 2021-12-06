48615c9a463c793ab469513c24e5773d13f55329
Medioh <32582504+Mediocrates10@users.noreply.github.com>
2021-12-06 01:33:40 -0500

    Update sysreport.ps1

--- powershell/sysreport.ps1
+++ powershell/sysreport.ps1
@@ -1,84 +1,18 @@
+param ( $func ="" )
 echo "SYSTEM REPORT"
-echo "System Hardware Description"
-$hardware = foreach ($p in (get-ciminstance win32_computersystem)) {
-	new-object -typename psobject -property @{
-			Name = ($p).name
-			PrimaryOwnerName = ($p).primaryownername
-			Domain = ($p).domain
-			"TotalPhysicalMemory(GB)" = ($p).totalphysicalmemory / 1gb -as [int]
-			Model = ($p).model
-			Manufacturer = ($p).manufacturer
-		}
-
-	}
-$hardware | fl 
-
-echo "Operating System Information"
-$os = foreach ($p in (get-ciminstance win32_operatingsystem)) {
-	new-object -typename psobject -property @{
-			Name = ($p).name
-			Version = ($p).version
-		}
-
-	}
-$os | fl 
-
-echo "Processor Description"
-$cpu = foreach ($p in (get-ciminstance win32_processor)) {
-	new-object -typename psobject -property @{
-			"Speed(GHz)" = ($p).maxclockspeed
-			"NumberOfCores" = ($p).numberofcores
-			"L1CacheSize(MB)" = "data unavailable"
-			"L2CacheSize(MB)" = ($p).l2cachesize / 1024 
-			"L3CacheSize(MB)" = ($p).l3cachesize / 1024
-		}
-
-	}
-$cpu | fl
-
-echo "RAM Summary"
-$totalRAM = 0
-$memory = get-ciminstance win32_physicalmemoryarray
-foreach ($ram in $memory) {
-	$sticks = $ram|get-cimassociatedinstance -resultclassname CIM_PhysicalMemory
-	$ramTable = foreach ($s in $sticks) {
-		
-		new-object -typename psobject -property @{
-			"Size(GB)" = ($s).capacity / 1gb -as [int]
-			Vendor = ($s).manufacturer
-			Description = ($s).description
-			Bank = ($s).banklabel
-			
-		}
-		$totalRAM = $totalRAM + ($s).capacity / 1gb -as [int]
-	}	
+if ($func -eq "system" -or $func -eq "") {
+	os_info
+	processor_info
+	ram_info
+	graphics_info
 }
-$ramTable | ft -autosize
-echo "Total RAM(GB):"
-echo $totalRAM
 
-echo "Network Adapter Configuration"
-$nacTable = foreach ($c in (get-ciminstance win32_networkadapterconfiguration | where ipenabled )) {
-        new-object -typename psobject -property @{
-            	Index = ($c).index
-        	Description = ($c).description
-        	Address = ($c).ipaddress
-        	Subnet = ($c).ipsubnet
-                DNSDomain = ($c).dnsdomain 
-		DNSServer = "data unavailable"
-    	}
+if ($func -eq "network" -or $func -eq "") {
+	nac_info
 }
-$nacTable | ft -autosize
 
-echo "Video Card Information"
-$gpu = foreach ($p in (get-ciminstance win32_videocontroller)) {
-	new-object -typename psobject -property @{
-			Vendor = ($p).name
-			Description = ($p).description
-			Resolution = "$(($p).currenthorizontalresolution) x $(($p).currentverticalresolution)"
-		}
-	}
-$gpu | fl 
-
+if ($func -eq "disks" -or $func -eq "") {
+	disks_info
+}
 
 
